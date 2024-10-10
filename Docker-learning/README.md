ed60d1344c194a981d75bfe7ae8b07560f07a48f
AmirDocs <amirbile@hotmail.co.uk>
2024-10-10 01:30:45 +0100

    commit Dockerfile & app.py incomplete

--- Docker-learning/README.md
+++ Docker-learning/README.md
@@ -10,18 +10,32 @@ Command | Description |
 `docker rm` |	Remove a container. |
 `docker ps` |	List running containers. |
 `docker cp` |	Copy files to/from a container. |
-`docker logs <container_name>` | View the logs (stdout) of a container. To follow logs, add the `-f` option before the container name. |
+`docker logs <container_name>` | View the logs (stdout) of a container. To follow logs, add the `-f` option before the container name. Use to debug.
 
-## **Docker Task**
+# **Docker Task**
 
-### **Phase 1**:
+## **Phase 1**:
 Created a simple python web application using flask and ran it on my local machine. This gives a clear understanding of how the application works before containerisation with Docker.
 
-### **Phase 2**:
-Created a Dockerfile to initiate the build process, install flask into container and access it on port 5002 from a local host.
+![python web app - flask](https://github.com/user-attachments/assets/4982a55d-cf6a-4e0b-8f76-7160d56b5baa)
 
-### **Phase 3**:
-Initiated Dockerfile to be run as a container. 
+
+## **Phase 2**:
+Created a Dockerfile to initiate the build process, install flask into container and access it on port 5002 from a local host. Initiated Dockerfile to be ran as a container.
+
+![Dockerfile 1](https://github.com/user-attachments/assets/5b995dd9-6c0d-456d-99bf-000cd594386a)
+
+
+## **Phase 3**:
+Connected flask app to mySQL database. Created a custom network with (my-custom network) in the terminal.
+
+![Linking MySQL to flask](https://github.com/user-attachments/assets/42175ead-7e4c-4106-a13a-2dce6e58459f)
+
+
+## **Phase 4**: 
+Build Docker image for the flask app with the updated docker file.
+
+
 
 
 
--- Docker-learning/hello_flask/app.py
+++ Docker-learning/hello_flask/app.py
@@ -1,16 +1,24 @@
 # app.py
 
-# Creating a new flask application instance (importing flask)
 from flask import Flask
+import MySQLdb
 
 app = Flask(__name__)
 
-# Setting a route, defining a route for the root URL /, returning Hello World!
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="mydb",    # Hostname of the MySQL container
+        user="root",    # Username to connect to MySQL
+        passwd="my-secret-pw",  # Password for the MySQL user
+        db="mysql"      # Name of the database to connect to
+    )                                                              # After establishing a connection, query below.
+    cur = db.cursor()
+    cur.execute("SELECT VERSION()")
+    version = cur.fetchone()
+    return f'Hello, World! MySQL version: {version[0]}'
 
-# Running application by doing app.run (running server). Using the app variable and telling it to run on host and port 5000.
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
 
@@ -19,3 +27,28 @@ if __name__ == '__main__':
 
 
 # Phase 1 - Created a simple python web application using flask and ran it on my local machine. This gives a clear understanding of how the application works before containerisation with Docker.
+# Creating a new flask application instance (importing flask) with from.
+# Setting a route @app.route, defining a route for the root URL /, returning Hello World!
+# # Running application by doing app.run (running server). Using the app variable and telling it to run on host and port 5000.
+
+
+
+
+
+# Phase 3 - Linking containers together
+# Linking the flask application to a mySQL database container, which enables us to execute SQL commands within the python application.
+# installs flask with mySQL client package. This provides the tools needed to connect to a mySQL database from within python app.
+#                   `docker network create my-custom-network' Entered in the terminal creates a custom network.
+# Setting network to my-custom-network to connect mySQL to network, adding '-e MYSQL_ROOT_PASSWORD=my-secret-pw' for authentication to database and specifying the version with mysql:5.7.
+#                   'docker run -d --name mydb --network my-custom-network -e MYSQL_ROOT_PASSWORD=my-secret-pw mysql:8'
+
+
+
+
+
+
+
+# PHASE 4 - Build Docker image for the flask app with the updated docker file.
+#                  'docker build -t hello-flask-mysql .'  (.) for the docker file.
+#                  'docker run -d --name myapp55 --network my-custom-network -p 5002:5002 hello-flask-mysql'
+#  Allocated to `my app2` as `myapp` already exists for previous container.
